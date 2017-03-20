8ea5309f84acc4fe1477544da640603e8bb22e2f
kudotakuya <ttt.ttt.7733@gmail.com>
2017-03-20 16:42:44 +0900

    デモ用完成

--- easydesu/FormViewController.swift
+++ easydesu/FormViewController.swift
@@ -7,142 +7,46 @@
 //
 
 import UIKit
-import GoogleAPIClientForREST
-import GTMOAuth2
 
 
-class FormViewController: UIViewController {
+class FormViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    private let kKeychainItemName = "Google Sheets API"
-    private let kClientID = "257780597942-94ebcpvamei8jvq65kks18a3fa24613f.apps.googleusercontent.com"
-    
-    private let scopes = [kGTLRAuthScopeSheetsSpreadsheetsReadonly]
-    
-    private let service = GTLRSheetsService()
-    let output = UITextView()
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var mypicker: UIPickerView!
+    @IBOutlet weak var commentField: UITextField!
 
-
+     var categoryArr: NSArray = ["住所","メニュー","写真","コメント"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mypicker.delegate = self
+        mypicker.dataSource = self
+        
 
-        output.frame = view.bounds
-        output.isEditable = false
-        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
-        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        
-        view.addSubview(output);
-        
-        if let auth = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychain(
-            forName: kKeychainItemName,
-            clientID: kClientID,
-            clientSecret: nil) {
-            service.authorizer = auth
-        }
-        
         // Do any additional setup after loading the view.
     }
     
-//    override func viewDidAppear(_ animated: Bool) {
-//        if let authorizer = service.authorizer,
-//            let canAuth = authorizer.canAuthorize, canAuth {
-//            listMajors()
-//        } else {
-//            present(
-//                createAuthController(),
-//                animated: true,
-//                completion: nil
-//            )
-//        }
-//    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryArr.count
+    }
+    
+    //表示内容
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryArr[row] as? String
+    }
+    
+    //選択時
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("列: \(row)")
+        print("値: \(categoryArr[row])")
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+  
 
-    func listMajors() {
-        output.text = "Getting sheet data..."
-        let spreadsheetId = "1twaJJZaNuKZKTuoPhWJb1MvcPHbAVUmwrSl_5R8I00I"
-        let range = "Class Data!A2:E"
-        let query = GTLRSheetsQuery_SpreadsheetsValuesGet
-            .query(withSpreadsheetId: spreadsheetId, range:range)
-        service.executeQuery(query,
-                             delegate: self,
-                             didFinish: "displayResultWithTicket:finishedWithObject:error:"
-        )
-    }
-    
-    // Process the response and display output
-    func displayResultWithTicket(ticket: GTLRServiceTicket,
-                                 finishedWithObject result : GTLRSheets_ValueRange,
-                                 error : NSError?) {
-        
-        if let error = error {
-            showAlert(title: "Error", message: error.localizedDescription)
-            return
-        }
-        
-        var majorsString = ""
-        let rows = result.values!
-        
-        if rows.isEmpty {
-            output.text = "No data found."
-            return
-        }
-        
-        majorsString += "Name, Major:\n"
-        for row in rows {
-            let name = row[0]
-            let major = row[4]
-            
-            majorsString += "\(name), \(major)\n"
-        }
-        
-        output.text = majorsString
-    }
-    
-    
-    
-    // Creates the auth controller for authorizing access to Google Sheets API
-//    private func createAuthController() -> GTMOAuth2ViewControllerTouch {
-//        let scopeString = scopes.joined(separator: " ")
-//        return GTMOAuth2ViewControllerTouch(
-//            scope: scopeString,
-//            clientID: kClientID,
-//            clientSecret: nil,
-//            keychainItemName: kKeychainItemName,
-//            delegate: self,
-//            finishedSelector: "viewController:finishedWithAuth:error:"
-//        )
-//    }
-//    
-    // Handle completion of the authorization process, and update the Google Sheets API
-    // with the new credentials.
-    func viewController(vc : UIViewController,
-                        finishedWithAuth authResult : GTMOAuth2Authentication, error : NSError?) {
-        
-        if let error = error {
-            service.authorizer = nil
-            showAlert(title: "Authentication Error", message: error.localizedDescription)
-            return
-        }
-        
-        service.authorizer = authResult
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // Helper for showing an alert
-    func showAlert(title : String, message: String) {
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: UIAlertControllerStyle.alert
-        )
-        let ok = UIAlertAction(
-            title: "OK",
-            style: UIAlertActionStyle.default,
-            handler: nil
-        )
-        alert.addAction(ok)
-        present(alert, animated: true, completion: nil)
-    }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -151,27 +55,29 @@ class FormViewController: UIViewController {
     
     @IBAction func sendButton(_ sender: Any) {
         // create the url-request
+        print(nameField.text!)
         let urlString = "https://sheets.googleapis.com/v4/spreadsheets/1twaJJZaNuKZKTuoPhWJb1MvcPHbAVUmwrSl_5R8I00I:batchUpdate"
-        var request = NSMutableURLRequest(url: NSURL(string: urlString)! as URL)
+        let request = NSMutableURLRequest(url: NSURL(string: urlString)! as URL)
         
         // set the method(HTTP-POST)
         request.httpMethod = "POST"
         // set the header(s)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer ya29.GlwUBKbBcnfUWUidpkJaSN_S0RdlJ04CFrpGk6-qKCm_LsT44wUNVHZ8A-dFEJNLeF6lefmPgm9PgzBYWBxBCT6g0bY9vmCOcE80P6Ls2eXzbHhVxKJKEQDfF5IIjg", forHTTPHeaderField: "Authorization")
         
         // set the request-body(JSON)
-        var params: [String: Dictionary] = [
+        let params: [String: Dictionary] = [
             "requests":[
                 "updateCells":[
                     "start":[
                         "sheetId": 0,
-                        "rowIndex": 2,
+                        "rowIndex": 45,
                         "columnIndex": 1
                         ],
                     "rows":[
                         "values":[
                             "userEnteredValue":[
-                                "stringValue": "国語"
+                                "stringValue": "\(nameField.text!)"
                                 ]
                             ]
                         ],
---
+++ easydesu/SheetViewController.swift
@@ -0,0 +1,148 @@
+import GoogleAPIClientForREST
+import GTMOAuth2
+import UIKit
+
+class SheetViewController: UIViewController {
+    
+    private let kKeychainItemName = "Google Sheets API"
+    private let kClientID = "944881284242-rmvknjbenlp8787jkkpos5mji7o38i7b.apps.googleusercontent.com"
+    
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    private let scopes = [kGTLRAuthScopeSheetsSpreadsheetsReadonly]
+    
+    private let service = GTLRSheetsService()
+    let output = UITextView()
+    
+    // When the view loads, create necessary subviews
+    // and initialize the Google Sheets API service
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        output.frame = view.bounds
+        output.isEditable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        view.addSubview(output);
+        
+        if let auth = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychain(
+            forName: kKeychainItemName,
+            clientID: kClientID,
+            clientSecret: nil) {
+            service.authorizer = auth
+        }
+        
+    }
+    
+    // When the view appears, ensure that the Google Sheets API service is authorized
+    // and perform API calls
+    override func viewDidAppear(_ animated: Bool) {
+        if let authorizer = service.authorizer,
+            let canAuth = authorizer.canAuthorize, canAuth {
+            listMajors()
+        } else {
+            present(
+                createAuthController(),
+                animated: true,
+                completion: nil
+            )
+        }
+    }
+    
+    // Display (in the UITextView) the names and majors of students in a sample
+    // spreadsheet:
+    // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    func listMajors() {
+        output.text = "Getting sheet data..."
+        let spreadsheetId = "1twaJJZaNuKZKTuoPhWJb1MvcPHbAVUmwrSl_5R8I00I"
+        let range = "Class Data!A2:E"
+        let query = GTLRSheetsQuery_SpreadsheetsValuesGet
+            .query(withSpreadsheetId: spreadsheetId, range:range)
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinish: "displayResultWithTicket:finishedWithObject:error:"
+        )
+    }
+    
+    // Process the response and display output
+    func displayResultWithTicket(ticket: GTLRServiceTicket,
+                                 finishedWithObject result : GTLRSheets_ValueRange,
+                                 error : NSError?) {
+        
+        if let error = error {
+            showAlert(title: "Error", message: error.localizedDescription)
+            return
+        }
+        
+        var majorsString = ""
+        let rows = result.values!
+        
+        if rows.isEmpty {
+            output.text = "No data found."
+            return
+        }
+        
+        majorsString += "Name, Major:\n"
+        for row in rows {
+            let name = row[0]
+            let major = row[4]
+            
+            majorsString += "\(name), \(major)\n"
+        }
+        
+        output.text = majorsString
+    }
+    
+    
+    
+    // Creates the auth controller for authorizing access to Google Sheets API
+    private func createAuthController() -> GTMOAuth2ViewControllerTouch {
+        let scopeString = scopes.joined(separator: " ")
+        print("auth")
+        return GTMOAuth2ViewControllerTouch(
+            scope: scopeString,
+            clientID: kClientID,
+            clientSecret: nil,
+            keychainItemName: kKeychainItemName,
+            delegate: self,
+            finishedSelector: Selector(("sheetviewController:finishedWithAuth:error:"))
+        )
+    }
+    
+    // Handle completion of the authorization process, and update the Google Sheets API
+    // with the new credentials.
+    func sheetviewController(vc : UIViewController,
+                        finishedWithAuth authResult : GTMOAuth2Authentication, error : NSError?) {
+        print("finish")
+        if let error = error {
+            service.authorizer = nil
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            return
+        }
+        
+        service.authorizer = authResult
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
